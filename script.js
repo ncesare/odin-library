07b5ecf0c8627fbfe8f5f0fe4de0d7e46626bcb0
Nick Cesare <ncesare@gmail.com>
2022-09-25 12:36:19 -0600

    Adding books adds element to div.shelf

--- script.js
+++ script.js
@@ -1,25 +1,52 @@
-function Book(title, author, pages, read) {
-    this.title = 'test';
-    this.author = author;
-    this.pages = pages;
-    this.read = read;
-    this.info = function() {
-        if (this.read == true) return `${title} by ${author} is ${pages} long and you have read it.`;
-        else if (this.read == false) return `${title} by ${author} is ${pages} long and you have not read it yet.`;
-        else return `${this.title} by ${this.author} is ${this.pages} long.`
-    }
-}
+const shelf = document.querySelector('#shelf');
 
 const textInputs = document.querySelectorAll('input[type="text"]');
 const readInput = document.querySelector('#read');
 
 const addBook = document.querySelector('#add-book');
+
 addBook.addEventListener('click', () => {
     const newBook = new Book();
     textInputs.forEach(input => newBook[input.id] = input.value);
     newBook.read = readInput.checked;
-    console.log(newBook.info());
+    addNewBook(newBook);
+    clear();
 })
+
+document.querySelector('#clear').addEventListener('click', () => clear());
+
+function Book(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+    this.info = function() {
+        return `${this.title} by ${this.author} is ${this.pages} long.`
+    }
+}
+
+function clear() {
+    textInputs.forEach(input => input.value = '');
+    readInput.checked = false;
+}
+
+function addNewBook(newBook) {
+    const bookContainer = document.createElement('div');
+
+    const bookTitle = document.createElement('div');
+    const bookAuthor = document.createElement('div');
+    const bookPages = document.createElement('div');
+
+    bookTitle.textContent = newBook.title;
+    bookAuthor.textContent = newBook.author;
+    bookPages.textContent = newBook.pages;
+
+    bookContainer.classList.add('book');
+    shelf.append(bookContainer);
+    bookContainer.append(bookTitle, bookAuthor, bookPages);
+}
+
+
 
 
 
