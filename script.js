2bba252fcbbccf4a2753fa650e8f21f8ee0adeb3
Nick Cesare <ncesare@gmail.com>
2022-09-25 11:54:59 -0600

    User inputs create new book object

--- script.js
+++ script.js
@@ -1,13 +1,27 @@
 function Book(title, author, pages, read) {
-    this.title = title;
+    this.title = 'test';
     this.author = author;
     this.pages = pages;
     this.read = read;
     this.info = function() {
-        if (read == true) return `${title} by ${author} is ${pages} long and you have read it.`;
-        else if (read == false) return `${title} by ${author} is ${pages} long and you have not read it yet.`;
+        if (this.read == true) return `${title} by ${author} is ${pages} long and you have read it.`;
+        else if (this.read == false) return `${title} by ${author} is ${pages} long and you have not read it yet.`;
+        else return `${this.title} by ${this.author} is ${this.pages} long.`
     }
 }
+
+const textInputs = document.querySelectorAll('input[type="text"]');
+const readInput = document.querySelector('#read');
+
+const addBook = document.querySelector('#add-book');
+addBook.addEventListener('click', () => {
+    const newBook = new Book();
+    textInputs.forEach(input => newBook[input.id] = input.value);
+    newBook.read = readInput.checked;
+    console.log(newBook.info());
+})
+
+
 
 // Quick test of the object constructor
 
